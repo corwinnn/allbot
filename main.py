69a7a50215109c3750aafbdfe1670d4f78c6ce73
Mike Heller <gellermish@yandex.ru>
2020-03-18 02:45:49 +0300

    webhook

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import config
 import json
+import logging
 import telebot
 import os
 
+from flask import Flask, request
+
+
 bot = telebot.TeleBot(config.token)
+server = Flask(__name__)
+
+logger = telebot.logger
+telebot.logger.setLevel(logging.INFO)
 
 with open('chats.json') as f:
     chat_user = json.load(f)
@@ -15,7 +23,8 @@ def command_help(message):
     uname = message.from_user.username
     if str(cid) not in chat_user:
         chat_user[str(cid)] = []
-    chat_user[str(cid)].append('@' + uname)
+    if '@' + uname not in chat_user[str(cid)]:
+        chat_user[str(cid)].append('@' + uname)
     bot.send_message(cid, 'Added ' + uname)
 
 
@@ -27,5 +36,18 @@ def command_help(message):
         json.dump(chat_user, f)
 
 
-if __name__ == '__main__':
-    bot.polling(none_stop=True, interval=0)
+@server.route('/' + config.token, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://your_heroku_project.com/' + config.token)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
