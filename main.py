efac3ad46d46609427e769175b92e43ea6d2359a
Mike Heller <gellermish@yandex.ru>
2020-03-18 22:31:59 +0300

    users can provide custom subgroups of members in chats

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ telebot.logger.setLevel(logging.INFO)
 with open('chats.json') as f:
     chat_user = json.load(f)
 
+with open('groups.json') as f:
+    chat_group = json.load(f)
+
 
 @bot.message_handler(commands=['add'])
 def command_help(message):
@@ -34,6 +37,42 @@ def command_help(message):
     bot.send_message(cid, ' '.join(list(chat_user[str(cid)])))
     with open('chats.json', 'w') as f:
         json.dump(chat_user, f)
+
+
+@bot.message_handler(commands=['group'])
+def command_help(message):
+    msg = bot.reply_to(message, "Name group and members. Your answer should be like group_name @member1 @member2...")
+    bot.register_next_step_handler(msg, process_group_name)
+
+
+def process_group_name(message):
+    cid = message.chat.id
+    text = message.text.split()
+    if len(text) == 1:
+        bot.send_message(cid, "No members")
+        return
+    if len(text) > 10:
+        bot.send_message(cid, "Too many members")
+        return
+
+    if str(cid) not in chat_group:
+        chat_group[str(cid)] = {}
+    chat_group[str(cid)][text[0]] = []
+    for user in text[1:]:
+        if user in chat_user[str(cid)]:
+            chat_group[str(cid)][text[0]].append(user)
+        else:
+            bot.send_message(cid, "I don't know this guy: " + user)
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    cid = message.chat.id
+    text = message.text
+    if message.text in chat_group[str(cid)]:
+        bot.send_message(cid, ' '.join(list(chat_group[str(cid)][text])))
+        with open('groups.json.json', 'w') as f:
+            json.dump(chat_group, f)
 
 
 @server.route('/' + config.token, methods=['POST'])
