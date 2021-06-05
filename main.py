aa09b1d8e8a6d5863168bdceebb646eac6eee942
corwinnn <geller.m@phystech.edu>
2021-06-05 18:16:58 +0300

    adapt functions for postgres

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ from db import SQLighter
 TOKEN = os.environ.get('BOT_TOKEN')
 APPLINK = os.environ.get('LINK')
 
+ALL_ALIAS = os.environ.get('ALL_ALIAS')
+
 bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
 
@@ -20,114 +22,100 @@ db = SQLighter()
 
 def add_user(func):
     def wrapper(*args, **kwargs):
-        print('1q')
         message = args[0]
-        print('2q')
         uid = message.from_user.id
+        cid = message.chat.id
         name = message.from_user.username
-        print('3q')
         if not db.subscriber_exists(uid):
-            print('4q')
             db.add_user(uid, name)
-            print('5q')
-        return func(*args, **kwargs)
+        if cid < 0:
+            db.add_member(cid, '@' + name, ALL_ALIAS)
+            return func(*args, **kwargs)
+        else:
+            bot.send_message(cid, 'I was born for group chats, please use me only in groups.')
+
     return wrapper
-
-
-with open('chats.json') as f:
-    chat_user = json.load(f)
-
-with open('groups.json') as f:
-    chat_group = json.load(f)
 
 
 @bot.message_handler(commands=['add'])
 @add_user
 def command_help(message):
-    cid = message.chat.id
-    uname = message.from_user.username
-    if str(cid) not in chat_user:
-        chat_user[str(cid)] = []
-    if '@' + uname not in chat_user[str(cid)]:
-        chat_user[str(cid)].append('@' + uname)
-    bot.send_message(cid, 'Added ' + uname)
+    msg = bot.reply_to(message, "Name members you want to be mentioned after /all command. Your answer should be like "
+                                "@member1 @member2...")
+    bot.register_next_step_handler(msg, process_members)
 
 
 @bot.message_handler(commands=['all'])
 @add_user
-def command_help(message):
+def command_all(message):
     cid = message.chat.id
-    if str(cid) in chat_user:
-        bot.send_message(cid, ' '.join(list(chat_user[str(cid)])))
-    with open('chats.json', 'w') as f:
-        json.dump(chat_user, f)
+    members = db.get_alias_list(cid, ALL_ALIAS)
+    if members:
+        bot.send_message(', '.join(members))
 
 
-@bot.message_handler(commands=['info'])
-@add_user
-def command_help(message):
-    cid = message.chat.id
-    greet = 'Nice chat! I know them: '
-    if str(cid) in chat_user:
-        people = ' '.join(list(chat_user[str(cid)])) + '\n' + 'Groups:\n'
-    else:
-        people = 'Nobody' + '\n' + 'Groups:\n'
-    if str(cid) in chat_group:
-        groups = '\n'.join([(g + ': ' + ' '.join(list(chat_group[str(cid)][g]))) for g in chat_group[str(cid)]])
-    else:
-        groups = 'Nothing'
-    bot.send_message(cid, greet + people + groups)
+# @bot.message_handler(commands=['info'])
+# @add_user
+# def command_help(message):
+#     cid = message.chat.id
+#     greet = 'Nice chat! I know them: '
+#     if str(cid) in chat_user:
+#         people = ' '.join(list(chat_user[str(cid)])) + '\n' + 'Groups:\n'
+#     else:
+#         people = 'Nobody' + '\n' + 'Groups:\n'
+#     if str(cid) in chat_group:
+#         groups = '\n'.join([(g + ': ' + ' '.join(list(chat_group[str(cid)][g]))) for g in chat_group[str(cid)]])
+#     else:
+#         groups = 'Nothing'
+#     bot.send_message(cid, greet + people + groups)
 
 
 @bot.message_handler(commands=['group'])
 @add_user
 def command_help(message):
-    msg = bot.reply_to(message, "Name group and members. Your answer should be like group_name @member1 @member2...")
+    msg = bot.reply_to(message, "Name group and members. Your answer should be like group_name @member1 @member2 ...")
     bot.register_next_step_handler(msg, process_group_name)
 
 
-@bot.message_handler(commands=['log'])
 @add_user
-def command_help(message):
-    print('6q')
-    bot.send_message(message.chat.id, str(chat_group))
-    bot.send_message(message.chat.id, str(chat_user))
-
-
 def process_group_name(message):
     cid = message.chat.id
     text = message.text.split()
     if len(text) == 1:
         bot.send_message(cid, "No members")
         return
-    if len(text) > 10:
-        bot.send_message(cid, "Too many members")
+    alias = text[0]
+    names = text[1:]
+    if any([name[0] != '@' for name in names]):
+        bot.send_message(cid, "Please, list like this: group_name @member1 @member2 ...")
         return
 
-    if str(cid) not in chat_group:
-        chat_group[str(cid)] = {}
-    chat_group[str(cid)][text[0]] = []
-    for user in text[1:]:
-        if user in chat_user[str(cid)]:
-            chat_group[str(cid)][text[0]].append(user)
-        else:
-            bot.send_message(cid, "I don't know this guy: " + user)
+    db.create_alias(cid, alias, names)
+
+
+@add_user
+def process_members(message):
+    cid = message.chat.id
+    names = message.text.split()
+    if any([name[0] != '@' for name in names]):
+        bot.send_message(cid, "Please, list like this: @member1 @member2 ...")
+        return
+
+    for name in names:
+        db.add_member(cid, '@' + name, ALL_ALIAS)
 
 
 @bot.message_handler(content_types=['text'])
 @add_user
 def get_text_messages(message):
     cid = message.chat.id
-    text = message.text
-    if str(cid) in chat_group and text in chat_group[str(cid)]:
-        bot.send_message(cid, ' '.join(list(chat_group[str(cid)][text])) or 'Empty group')
-        with open('groups.json', 'w') as f:
-            json.dump(chat_group, f)
-    uname = message.from_user.username
-    if str(cid) not in chat_user:
-        chat_user[str(cid)] = []
-    if '@' + uname not in chat_user[str(cid)]:
-        chat_user[str(cid)].append('@' + uname)
+    words = message.text.split()
+    aliases = [w for w in words if w.startswith('@@')]
+    if aliases:
+        for alias in aliases:
+            members = db.get_alias_list(cid, alias)
+            if members:
+                bot.send_message(', '.join(members))
 
 
 @server.route('/' + TOKEN, methods=['POST'])
