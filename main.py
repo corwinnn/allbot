53c7b7057f9e17d67793239ad3701a1a74b4724c
corwinnn <geller.m@phystech.edu>
2021-06-06 11:27:55 +0300

    add help message

--- main.py
+++ main.py
@@ -4,13 +4,9 @@ import telebot
 
 from flask import Flask, request
 
+from config import TOKEN, ALL_ALIAS, ALIAS_START, APPLINK, HELLO_TEXT
 from db import SQLighter
 
-TOKEN = os.environ.get('BOT_TOKEN')
-APPLINK = os.environ.get('LINK')
-
-ALL_ALIAS = os.environ.get('ALL_ALIAS')
-ALIAS_START = '@'
 
 bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
@@ -19,15 +15,6 @@ logger = telebot.logger
 telebot.logger.setLevel(logging.INFO)
 
 db = SQLighter()
-
-HELLO_TEXT = ("Hi! I want to help you to call your friends in group chats.\n\n"
-           "/all or @all - I'll tag all known members of the chat (I know members that have written something after adding me "
-           "to the chat or people that were added by /add command.\n\n"
-           "/add - add people manually\n\n"
-           "/group - add alias for a group. For example, you'll be able to call all boys in the chat by @boys, and all girls by @girls\n\n"
-           "/info - learn about current known members and aliases\n\n"
-           "For сomplaints and suggestions - write to @MikeHeller\n\n"
-           "Public code repository - https://github.com/corwinnn/allbot")
 
 
 def add_user(func):
@@ -67,9 +54,7 @@ def send_help(cid):
     Send help message
     :param cid: telegram chat id
     """
-    text = 
-
-    bot.send_message(cid, text)
+    bot.send_message(cid, HELLO_TEXT)
 
 
 @bot.message_handler(commands=['add'])
